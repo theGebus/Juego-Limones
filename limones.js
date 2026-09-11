4453f8fc887c36e8a03662c4aa1e17ce8816a611
theGebus <diegoaor24@gmail.com>
2026-09-10 22:48:25 -0500

    Agrego la funcion mover a la derecha y izquierda

--- limones.js
+++ limones.js
@@ -4,6 +4,8 @@ let ctx=canvas.getContext("2d");
 const alturaSuelo=25;
 const altura_Personaje=35;
 const ancho_Personaje=20;
+let personajeX=canvas.width/2;
+let personajeY=height; 
 
 function llamarFunciones (){
     dibujarSuelo();
@@ -18,6 +20,32 @@ function dibujarSuelo (){
 function dibujarPersonaje (){
 
     ctx.fillStyle="red";
-    ctx.fillRect(canvas.width/2,canvas.height-(alturaSuelo+altura_Personaje ),ancho_Personaje,altura_Personaje)
+    ctx.fillRect(personajeX,canvas.height-(alturaSuelo+altura_Personaje ),ancho_Personaje,altura_Personaje)
 
+}
+
+//Mover a la Izquierda
+
+function moverIzquierdas (){
+    personajeX=personajeX-10
+    cleanPersonaje();
+
+}
+
+function cleanPersonaje (){
+    limpiarCanva();
+    dibujarPersonaje();
+    dibujarSuelo();
+
+}
+
+function limpiarCanva (){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+
+//Mover a la Derecha
+
+function moverDerecha (){
+    personajeX=personajeX+10;
+    cleanPersonaje();
 }
