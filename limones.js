222b1feef4c7f8a6b863a451add9d4476d51ccff
theGebus <diegoaor24@gmail.com>
2026-09-10 22:12:39 -0500

    Agrego  el suelo y la creacion del personaje

--- limones.js
+++ limones.js
@@ -1,2 +1,23 @@
-let canvas=document.getElementById("areaJuego")
-let ctx=canvas.getContext("2d")
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+
+const alturaSuelo=25;
+const altura_Personaje=35;
+const ancho_Personaje=20;
+
+function llamarFunciones (){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo (){
+    ctx.fillStyle="green";
+    ctx.fillRect(0,canvas.height-alturaSuelo ,canvas.width,alturaSuelo);
+}
+
+function dibujarPersonaje (){
+
+    ctx.fillStyle="red";
+    ctx.fillRect(canvas.width/2,canvas.height-(alturaSuelo+altura_Personaje ),ancho_Personaje,altura_Personaje)
+
+}
